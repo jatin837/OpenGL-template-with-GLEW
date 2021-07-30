c59c80cf25601e7bc09709c7562943546468fcb5
dj4t9n <63631162+jatin837@users.noreply.github.com>
2021-07-30 15:17:18 +0530

    no need for readFile since i have read_shader function

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,6 @@
 
 std::string read_shader(std::string);
 
-std::string readFile(const char *filePath) {
-    std::string content;
-    std::ifstream fileStream(filePath, std::ios::in);
-
-    if(!fileStream.is_open()) {
-        std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
-        return "";
-    }
-
-    std::string line = "";
-    while(!fileStream.eof()) {
-        std::getline(fileStream, line);
-        content.append(line + "\n");
-    }
-
-    fileStream.close();
-    return content;
-}
 int main() {
   // start GL context and O/S window using the GLFW helper library
   if (!glfwInit()) {
